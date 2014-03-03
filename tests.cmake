84c4bf7bf4b3532f0d340cdd81977f1ad3320265
Aurélien Richez <aurelien.richez@inria.fr>
2014-03-03 17:46:21 +0100

    separated test in another file and make unit testing as a part of build it is possible to skip the test when generating the makefile with -DSKIPTEST=true

---
+++ tests.cmake
@@ -0,0 +1,42 @@
+## original test :
+
+file(GLOB_RECURSE src_thegame_test src/test/c/main.c src/test/c/Tests.c )
+
+add_executable(
+	test-thegame
+	${src_thegame_test}
+)
+
+file(COPY src/test/resources/data DESTINATION .)
+
+include_directories(src/test/include)
+
+target_link_libraries(test-thegame THEGAME)
+
+## unit testing
+if(NOT SKIPTEST AND NOT CMAKE_CROSSCOMPILING)
+
+find_package(Check)
+
+if(CHECK_FOUND)
+    enable_testing()
+    
+    add_executable(test_BeliefFromSensors src/test/c/checkBeliefFromSensors.c)
+    
+    target_link_libraries(test_BeliefFromSensors THEGAME ${CHECK_LIBRARIES})
+    
+    add_test(NAME test_BeliefFromSensors COMMAND test_BeliefFromSensors)
+    
+
+set(test_binaries test_BeliefFromSensors)
+
+add_custom_target(unit_test ALL  ctest --output-on-failure
+    DEPENDS THEGAME ${test_binaries}
+    COMMENT "\n\n   ===== TESTS =====   \n\n"
+    )    
+
+else(CHECK_FOUND)
+    message(WARNING "Check library not found, no test will be executed")
+endif(CHECK_FOUND)
+
+endif(NOT SKIPTEST AND NOT CMAKE_CROSSCOMPILING)
