d3b190adfaa74ce2c87790816890159532ab1c39
Aurélien Richez <aurelien.richez@inria.fr>
2014-03-06 11:01:44 +0100

    created new sensor configuration to be independent from the original test

--- src/test/c/checkBeliefFunctions.c
+++ src/test/c/checkBeliefFunctions.c
@@ -12,15 +12,15 @@
 #include "BeliefsFromSensors.h"
 #include "unit_tests.h"
 
-#define SENSOR_NB 5
+#define SENSOR_NB 2
 
 #define SET_ELEMENT_DECLARE(name, val1, val2, val3) \
 	static char name ##  _VAL[] = {val1, val2, val3}; \
 	static Sets_Element name = {name ##  _VAL, val1 + val2 + val3}
 
-SET_ELEMENT_DECLARE(AKA, 1,0,0);
-SET_ELEMENT_DECLARE(BEA, 0,1,0);
-SET_ELEMENT_DECLARE(ELF, 0,0,1);
+SET_ELEMENT_DECLARE(A, 1,0,0);
+SET_ELEMENT_DECLARE(B, 0,1,0);
+SET_ELEMENT_DECLARE(C, 0,0,1);
 SET_ELEMENT_DECLARE(VOID, 0,0,0);
 
 
@@ -30,9 +30,9 @@ BF_BeliefFunction DempsterfusedBelief, SmetsFusedBelief;
 
 
 static void setup() {
-    char const *sensorTypes[] = {"S1", "S2", "S3", "S4", "S5"};
-    double sensorMeasures[] = {300.0, 400.0, 345.76, 315.58, 297.42};
-	beliefStructure = BFS_loadBeliefStructure("test");
+    char const *sensorTypes[] = {"S1", "S2"};
+    double sensorMeasures[] = {300.0, 400.0};
+	beliefStructure = BFS_loadBeliefStructure("unittest");
 
 	evidences = BFS_getEvidence(beliefStructure, sensorTypes, sensorMeasures, SENSOR_NB);
 	SmetsFusedBelief = BF_SmetsCombination(evidences[0], evidences[1]);
@@ -54,24 +54,40 @@ static void teardown() {
  * Manipulation Tests
  * ==================
  */
+
+/*
+ * ## getMax
+ */
+
 START_TEST(getMaxMassReturnsTheRightValues) {
-	/* The max for the first evidence function should be 0.75 for Aka*/
+	/* The max for the first evidence function should be 0.75 for {A}*/
 	BF_FocalElement element = BF_getMax(BF_M, evidences[0], 0,
 			Sets_generatePowerSet(evidences[0].elementSize));
 	float value = BF_M(evidences[0], element.element);
-	ck_assert(Sets_equals(element.element, AKA, 3));
+	ck_assert(Sets_equals(element.element, A, 3));
 	assert_flt_equals(0.75f, value, BF_PRECISION);
 	BF_freeBeliefPoint(&element);
 }
 END_TEST
 
 START_TEST(getMaxBetPReturnsTheRightValues) {
-	/* The max for the first evidence function should be 1.0 for {Aka u Bea u Elf} */
+	/* The max for the first evidence function should be 1.0 for {AuB} */
 	BF_FocalElement focalPoint = BF_getMax(BF_betP, evidences[0], 0,
 			Sets_generatePowerSet(evidences[0].elementSize));
 	float value = BF_betP(evidences[0], focalPoint.element);
 	assert_flt_equals(1.0f, value, BF_PRECISION);
-	ck_assert_msg(Sets_equals(focalPoint.element, Sets_union(AKA,BEA, 3),3),
+	ck_assert_msg(Sets_equals(focalPoint.element, Sets_union(A,B, 3),3),
+			"BetP did not return AKA u BEA");
+}
+END_TEST
+
+START_TEST(getMaxBetWithCardLimitReturnsTheRightValues) {
+	/* The max with 1 as card for the first evidence function should be 0.825 for {A} */
+	BF_FocalElement focalPoint = BF_getMax(BF_betP, evidences[0], 1,
+			Sets_generatePowerSet(evidences[0].elementSize));
+	float value = BF_betP(evidences[0], focalPoint.element);
+	assert_flt_equals(0.825f, value, BF_PRECISION);
+	ck_assert_msg(Sets_equals(focalPoint.element, A,3),
 			"BetP did not return AKA u BEA");
 }
 END_TEST
@@ -80,31 +96,34 @@ END_TEST
  * Fusion tests
  * ============
  */
+
+/* ##Smets */
 START_TEST(SmetsCombinationValuesAreOk) {
 	/*
 	 * expected values :
-	 * m(Aka) = 0.45
-	 * m(Bea) = 0.025
+	 * m(A) = 0.45
+	 * m(B) = 0.025
 	 * m(void) = 0.525
-	 * m(E) = 0
+	 * m(C) = 0
 	 */
-	assert_flt_equals(0.45f, BF_M(SmetsFusedBelief, AKA), BF_PRECISION);
-	assert_flt_equals(0.025f, BF_M(SmetsFusedBelief, BEA), BF_PRECISION);
-	assert_flt_equals(0.0f, BF_M(SmetsFusedBelief, ELF), BF_PRECISION);
+	assert_flt_equals(0.45f, BF_M(SmetsFusedBelief, A), BF_PRECISION);
+	assert_flt_equals(0.025f, BF_M(SmetsFusedBelief, B), BF_PRECISION);
+	assert_flt_equals(0.0f, BF_M(SmetsFusedBelief, C), BF_PRECISION);
 	assert_flt_equals(0.525f, BF_M(SmetsFusedBelief, VOID), BF_PRECISION);
 }
 END_TEST
 
+/* ##Dempster */
 START_TEST(DempsterCombinationValuesAreOk) {
 	/*
 	 * expected values :
-	 * m(Aka) = 0.45 / 0.475
-	 * m(Bea) = 0.025 / 0.475
+	 * m(A) = 0.45 / 0.475
+	 * m(B) = 0.025 / 0.475
 	 * m(AuB) = 0
-	 * m(E) = 0
+	 * m(C) = 0
 	 */
-	assert_flt_equals(0.45 / 0.475, BF_M(DempsterfusedBelief, AKA), BF_PRECISION);
-	assert_flt_equals(0.025 / 0.475, BF_M(DempsterfusedBelief, BEA), BF_PRECISION);
+	assert_flt_equals(0.45 / 0.475, BF_M(DempsterfusedBelief, A), BF_PRECISION);
+	assert_flt_equals(0.025 / 0.475, BF_M(DempsterfusedBelief, B), BF_PRECISION);
 	assert_flt_equals(0.0f, BF_M(DempsterfusedBelief, VOID), BF_PRECISION);
 }
 END_TEST
@@ -115,6 +134,7 @@ TCase* testCaseManipulation = tcase_create("Manipulation");
 tcase_add_checked_fixture(testCaseManipulation, setup, teardown);
 tcase_add_test(testCaseManipulation, getMaxMassReturnsTheRightValues);
 tcase_add_test(testCaseManipulation, getMaxBetPReturnsTheRightValues);
+tcase_add_test(testCaseManipulation, getMaxBetWithCardLimitReturnsTheRightValues);
 return testCaseManipulation;
 }
 
